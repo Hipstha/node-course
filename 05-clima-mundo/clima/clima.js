27a3b85c5951c5920983213c9f03a9841b5a9491
Alejandro Cruz Pérez <alex_crz97@hotmail.com>
2019-08-04 21:31:41 -0500

    Se carga la seccion 6

---
+++ 05-clima-mundo/clima/clima.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const getClima = async (lat, lng) => {
+  const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=14db6adba265ab237b944e95b2dbc264`);
+  return resp.data.main.temp;
+}
+
+module.exports = {
+  getClima
+}
