52aaa78805dbad7724c01065304a4a2bf32cbd3c
Alejandro Cruz Pérez <alex_crz97@hotmail.com>
2019-08-05 22:31:22 -0500

    RestServer - usuario

--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const app = express();
 
 const Usuario = require('../models/usuario');
 
 app.get('/usuario', (req, res) => {
-  res.json("get Usuario");
+
+  let desde = req.query.desde || 0;
+  desde = Number(desde);
+
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+
+  Usuario.find({estado: true}, 'nombre email role estado google img')
+    .skip(desde)
+    .limit(limite)
+    .exec((err, usuarios)=>{
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+      }
+
+      Usuario.count({estado: true}, (err, conteo)=>{
+        res.json({
+          ok: true,
+          usuarios,
+          cuantos: conteo
+        });
+      });
+
+    });
+
 });
 
 app.post('/usuario', (req, res)=>{
@@ -14,7 +43,7 @@ app.post('/usuario', (req, res)=>{
   let usuario = new Usuario({
     nombre: body.nombre,
     email: body.email,
-    password: body.password,
+    password: bcrypt.hashSync( body.password, 10),
     role: body.role
   });
 
@@ -25,6 +54,9 @@ app.post('/usuario', (req, res)=>{
         err
       });
     }
+
+    //usuarioDB.password = null
+
     res.json({
       ok: true,
       usuario: usuarioDB
@@ -34,13 +66,65 @@ app.post('/usuario', (req, res)=>{
 
 app.put('/usuario/:id', (req, res)=>{
   let id = req.params.id;
-  res.json({
-    id
+  let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+  Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, usuarioDB)=>{
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
   });
+
 });
 
-app.delete('/usuario', (req, res)=>{
-  res.json('delete Usuario');
+app.delete('/usuario/:id', function(req, res){
+  let id = req.params.id;
+  let cambiaEstado = {
+    estado: false
+  }
+
+  Usuario.findByIdAndUpdate(id, cambiaEstado, {new:true}, (err, usuarioDB)=>{
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
+  });
+
+  // Usuario.findByIdAndRemove(id, (err, usuarioBorrado)=>{
+  //   if(err){
+  //     return res.status(400).json({
+  //       ok: false,
+  //       err
+  //     });
+  //   }
+  //
+  //   if(!usuarioBorrado){
+  //     return res.status(400).json({
+  //       ok: false,
+  //       err:{
+  //         message: 'Usuario no encontrado'
+  //       }
+  //     })
+  //   }
+  //
+  //   res.json({
+  //     ok: true,
+  //     usuario: usuarioBorrado
+  //   })
+  // });
+
 });
 
 
--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -9,9 +9,11 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res)=>{
-  if(err) throw err;
-  console.log('Base de datos ONLINE');
+mongoose.connect(process.env.URLDB,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res)=>{
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, ()=>{
