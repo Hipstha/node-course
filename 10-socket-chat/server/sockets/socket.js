abae50f82dd4504b2296fdc8536dacee0aeee01a
Alejandro Cruz Pérez <alex_crz97@hotmail.com>
2019-08-15 18:56:23 -0500

    respaldo seccion 16

--- 10-socket-chat/server/sockets/socket.js
+++ 10-socket-chat/server/sockets/socket.js
@@ -8,18 +8,20 @@ io.on('connection', (client) => {
 
   client.on('entrarChat', (data, callback) => {
 
-    if(!data.nombre){
+    if(!data.nombre || !data.sala){
       return callback({
         error: true,
-        mensaje: 'El nombre es necesario'
+        mensaje: 'El nombre/sala es necesario'
       })
     }
 
-    let personas = usuarios.agregarPersona( client.id, data.nombre );
+    client.join(data.sala);
 
-    client.broadcast.emit('listaPersonas', usuarios.getPersonas());
+    usuarios.agregarPersona( client.id, data.nombre, data.sala );
 
-    callback(personas);
+    client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+
+    callback(usuarios.getPersonasPorSala(data.sala));
     //console.log(usuario);
   });
 
@@ -28,14 +30,14 @@ io.on('connection', (client) => {
     let persona = usuarios.getPersona(client.id);
 
     let mensaje = crearMensaje( persona.nombre, data.mensaje );
-    client.broadcast.emit( 'crearMensaje' , mensaje);
+    client.broadcast.to(persona.sala).emit( 'crearMensaje' , mensaje);
   });
 
   client.on('disconnect', () => {
 
     let personaBorrada = usuarios.borrarPersona( client.id );
-    client.broadcast.emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`));
-    client.broadcast.emit('listaPersonas', usuarios.getPersonas());
+    client.broadcast.to(personaBorrada).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`));
+    client.broadcast.to(personaBorrada).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
 
   });
 
