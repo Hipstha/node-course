9648b907b31aef23512bdc856fb76248421a2001
Alejandro Cruz Pérez <alex_crz97@hotmail.com>
2019-08-15 12:50:42 -0500

    Se agrega parte de seccion 16

---
+++ 10-socket-chat/public/js/socket-chat.js
@@ -0,0 +1,57 @@
+var socket = io();
+
+var params = new URLSearchParams( window.location.search );
+
+if(!params.has('nombre')){
+  window.location = 'index.html';
+  throw new Error('El nombre es necesario');
+}
+
+var usuario = {
+  nombre: params.get('nombre')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function( resp ){
+      console.log('Usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+//Escuchar cambios de usuarios, entrar y salir del chat
+
+socket.on('listaPersonas', function(personas) {
+
+    console.log(personas);
+
+});
+
+//Mensajes orivados
+
+socket.on('mensajePrivado', function( mensaje ){
+  console.log('Mensaje privado: ', mensaje);
+});
---
+++ 10-socket-chat/server/sockets/socket.js
@@ -0,0 +1,50 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades')
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+  client.on('entrarChat', (data, callback) => {
+
+    if(!data.nombre){
+      return callback({
+        error: true,
+        mensaje: 'El nombre es necesario'
+      })
+    }
+
+    let personas = usuarios.agregarPersona( client.id, data.nombre );
+
+    client.broadcast.emit('listaPersonas', usuarios.getPersonas());
+
+    callback(personas);
+    //console.log(usuario);
+  });
+
+  client.on('crearMensaje', (data) => {
+
+    let persona = usuarios.getPersona(client.id);
+
+    let mensaje = crearMensaje( persona.nombre, data.mensaje );
+    client.broadcast.emit( 'crearMensaje' , mensaje);
+  });
+
+  client.on('disconnect', () => {
+
+    let personaBorrada = usuarios.borrarPersona( client.id );
+    client.broadcast.emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`));
+    client.broadcast.emit('listaPersonas', usuarios.getPersonas());
+
+  });
+
+  //Mensajes privados
+  client.on('mensajePrivado', data => {
+
+    let persona = usuarios.getPersona( client.id );
+    client.broadcast.to(data.para).emit( 'mensajePrivado', crearMensaje(persona, data.mensaje) );
+
+  });
+
+});
